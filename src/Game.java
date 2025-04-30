a7c106e0ab5d07d9623581c5e900b35610409da0
DariusTitiriga <andreas.titiriga@gmail.com>
2025-04-30 17:46:52 +0200

    Initial commit

---
+++ src/Game.java
@@ -0,0 +1,14 @@
+public class Game {
+    public static void main(String[] args) {
+        // Task 1 Afiseaza un mesaj de welcome.
+        // Task 2 Salveaza intr o variabila un cuvant "avion".
+        // Task 3 Afiseaza la consola un sir de stelute care sa reprezinte literele din cuvant.
+        // Task 4 Cere de la user o litera apoi verifica daca litera este in cuvant.
+        // Task 5 Daca da inlocuieste steluta de la pozitia respectiva cu litera ghicita.
+        // Task 6 Daca nu sa se scada o viata dintre cele 10 vieti stabilite initial.
+        // Task 7 Introdu toata logica intr un grup incepand cu task ul 3.
+        // Task 8 Cand cuvantul este complet ghicit din litere afiseaza mesajul ai ghicit si opreste programul.
+        // Task 9 Cand numarul de viete ajunge la 0 afiseaza "Ai pierdut!" si opreste programul.
+
+    }
+}
