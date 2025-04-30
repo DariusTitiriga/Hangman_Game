d9ddd0de79faa1ce16943a5edd6c37d67ba3486b
DariusTitiriga <andreas.titiriga@gmail.com>
2025-04-30 18:29:25 +0200

    Added game logic

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Game {
     public static void main(String[] args) {
         // Task 1 Afiseaza un mesaj de welcome.
@@ -8,7 +12,35 @@ public class Game {
         // Task 6 Daca nu sa se scada o viata dintre cele 10 vieti stabilite initial.
         // Task 7 Introdu toata logica intr un grup incepand cu task ul 3.
         // Task 8 Cand cuvantul este complet ghicit din litere afiseaza mesajul ai ghicit si opreste programul.
-        // Task 9 Cand numarul de viete ajunge la 0 afiseaza "Ai pierdut!" si opreste programul.
+        // Task 9 Cand numarul de vieti ajunge la 0 afiseaza "Ai pierdut!" si opreste programul.
 
+        System.out.println("Welcome!");
+        int lives = 10;
+        String hiddenWord = "plane";
+        char[] stars = {'*', '*', '*', '*', '*'};
+        while (true) {
+            boolean letterIsFound = false;
+            System.out.println(stars);
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Please write a letter.");
+            char letter = scan.next().charAt(0);
+            for (int i = 0; i < hiddenWord.length(); i++) {
+                if (letter == hiddenWord.charAt(i)) {
+                    stars[i] = letter;
+                    letterIsFound = true;
+                }
+            }
+            if (!letterIsFound){
+                lives--;
+            }
+            if (!String.valueOf(stars).contains("*")){
+                System.out.println("You guessed the word!");
+                break;
+            }
+            if (lives == 0){
+                System.out.println("You didn't guessed the word! \nYou lost!");
+                break;
+            }
+        }
     }
 }
