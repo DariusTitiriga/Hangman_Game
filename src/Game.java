75b2ef849fef9277ec6848650a8191492e1509c4
DariusTitiriga <andreas.titiriga@gmail.com>
2025-05-12 17:47:31 +0200

    Added colors and random words.

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,7 @@
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Game {
@@ -14,16 +16,23 @@ public class Game {
         // Task 8 Cand cuvantul este complet ghicit din litere afiseaza mesajul ai ghicit si opreste programul.
         // Task 9 Cand numarul de vieti ajunge la 0 afiseaza "Ai pierdut!" si opreste programul.
 
-        System.out.println("Welcome!");
+        System.out.println(Colors.BLUE + "Welcome!" + Colors.RESET);
         int lives = 10;
-        String hiddenWord = "plane";
-        char[] stars = {'*', '*', '*', '*', '*'};
+        int attempts = 0;
+        ArrayList<String> words = new ArrayList<>(Arrays.asList("stereotype", "professor", "retreat", "branch",
+                "product", "business", "system", "customer", "season", "nightmare", "incredible"));
+        Random rnd = new Random();
+        int index = rnd.nextInt(words.size());
+        String hiddenWord = words.get(index);
+        char[] stars = new char[hiddenWord.length()];
+        Arrays.fill(stars, '*');
         while (true) {
             boolean letterIsFound = false;
             System.out.println(stars);
             Scanner scan = new Scanner(System.in);
-            System.out.println("Please write a letter.");
+            System.out.println(Colors.YELLOW + "Please write a letter." + Colors.RESET);
             char letter = scan.next().charAt(0);
+            attempts++;
             for (int i = 0; i < hiddenWord.length(); i++) {
                 if (letter == hiddenWord.charAt(i)) {
                     stars[i] = letter;
@@ -31,14 +40,16 @@ public class Game {
                 }
             }
             if (!letterIsFound){
+                System.out.println(Colors.RED_BRIGHT + "Wrong choice!" + Colors.RESET);
                 lives--;
             }
             if (!String.valueOf(stars).contains("*")){
-                System.out.println("You guessed the word!");
+                System.out.println(Colors.GREEN + "You guessed the word!" + Colors.RESET);
+                System.out.println("You guessed in " + attempts + " attempts!");
                 break;
             }
             if (lives == 0){
-                System.out.println("You didn't guessed the word! \nYou lost!");
+                System.out.println(Colors.RED + "You didn't guessed the word! \nYou lost!");
                 break;
             }
         }
